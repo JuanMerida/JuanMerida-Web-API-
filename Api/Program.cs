65d896a26b6a97a3197f0a2c21828d0391525846
JuanMerida <juan.meridaet12d1@gmail.com>
2024-10-14 09:27:35 -0300

    xd

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,14 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -28,60 +25,5 @@ app.MapGroup("/api")
     .MapRolEndPoints()
     .WithTags("rol");
 
-// app.MapPost("/usuario/{idUsuario}/rol/{idRol}", (int idUsuario, int idRol) =>{
-//     var usuario = usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-//     var rol = roles.FirstOrDefault(rol => rol.IdRol == idRol);
-
-//     if (rol != null &&  usuario!= null)
-//     {
-//         usuario.Roles.Add(rol);
-//         return Results.Ok();
-//     }
-
-//     return Results.NotFound();
-
-// })
-// .WithTags("usuario");
-
-// app.MapDelete("/usuario/{idUsuario}/rol/{idRol}",(int idUsuario, int idRol) =>{
-//     var usuario = usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-//     var rol = roles.FirstOrDefault(rol => rol.IdRol == idRol);
-
-//     if (rol != null &&  usuario!= null)
-//     {
-//         usuario.Roles.Remove(rol);
-//         return Results.Ok();
-//     }
-
-//     return Results.NotFound();
-// }).WithTags("usuario");
-
-//     app.MapPost("/rol/{idRol}/usuario/{idUsuario}", (int idUsuario, int idRol) =>{
-//     var usuario = usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-//     var rol = roles.FirstOrDefault(rol => rol.IdRol == idRol);
-
-//     if (rol != null &&  usuario!= null)
-//     {
-//         rol.Usuarios.Add(usuario);
-//         return Results.Ok();
-//     }
-
-//     return Results.NotFound();
-
-// })
-// .WithTags("rol");
-
-// app.MapDelete("/rol/{idRol}/usuario/{idUsuario}",(int idUsuario, int idRol) =>{
-//     var usuario = usuarios.FirstOrDefault(usuario => usuario.IdUsuario == idUsuario);
-//     var rol = roles.FirstOrDefault(rol => rol.IdRol == idRol);
-
-//     if (rol != null &&  usuario!= null)
-//     {
-//         rol.Usuarios.Remove(usuario);
-//         return Results.Ok();
-//     }
-
-//     return Results.NotFound();
-// }).WithTags("rol");
 
 app.Run();
